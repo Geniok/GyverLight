fb8e062e58a468c58e9140e07939ff43a63621ac
Vikharev Evgeniy <Genoik@yandex.ru>
2019-12-07 21:12:59 +0300

    Переписал программу по заданию Вадима

--- firmware/gyverLight_v1.4/gyverLight_v1.4.ino
+++ firmware/gyverLight_v1.4/gyverLight_v1.4.ino
@@ -1,38 +1,25 @@
 /*
   Скетч к проекту "Эффектный светильник"
-  Страница проекта (схемы, описания): https://alexgyver.ru/gyverlight/
-  Исходники на GitHub: https://github.com/AlexGyver/gyverlight/
-  Нравится, как написан код? Поддержи автора! https://alexgyver.ru/support_alex/
-  Автор: AlexGyver Technologies, 2018
-  https://AlexGyver.ru/
 */
 
 /*
    Управление кнопкой/сенсором
-  - Удержание - яркость
-  - 1х тап - вкл/выкл
-  - 2х тап - переключ режима
-  - 3х тап - вкл/выкл белый свет
-  - 4х тап - старт/стоп авто смены режимов
+  - 1х тап - вкл. воспроизведения голосового сообщения. Смена режима работы световых индикаторов 
 */
 
-/*
-   Версия 1.3 - пофикшен баг с fillAll
-*/
 // ************************** НАСТРОЙКИ ***********************
 #define CURRENT_LIMIT 2000 // лимит по току в миллиамперах, автоматически управляет яркостью (пожалей свой блок питания!) 0 - выключить лимит
-#define AUTOPLAY_TIME 30   // время между сменой режимов в секундах
+#define AUTOPLAY_TIME 10   // время проигрывания голосового сообщения
+#define SPEED_EFFECT 10    // Время отображения секущего эффекта
 
-#define NUM_LEDS 9       // количество светодиодов в одном отрезке ленты
-#define NUM_STRIPS 3     // количество отрезков ленты (в параллели)
-#define LED_PIN 6        // пин ленты
-#define BTN_PIN 2        // пин кнопки/сенсора
-#define MIN_BRIGHTNESS 5 // минимальная яркость при ручной настройке
-#define BRIGHTNESS 250   // начальная яркость
-#define FIRE_PALETTE 0   // разные типы огня (0 - 3). Попробуй их все! =)
+#define NUM_LEDS 9   // количество светодиодов в одном отрезке ленты
+#define NUM_STRIPS 3 // количество отрезков ленты (в параллели)
+#define LED_PIN 6    // пин ленты
+#define BTN_PIN 2    // пин кнопки/сенсора
 
-// ************************** ДЛЯ РАЗРАБОТЧИКОВ ***********************
-#define MODES_AMOUNT 6
+#define IDS1820_PIN 5 // пин isd1820
+
+#define BRIGHTNESS 255 // яркость
 
 #include "GyverButton.h"
 GButton touch(BTN_PIN, LOW_PULL, NORM_OPEN);
@@ -42,25 +29,20 @@ CRGB leds[NUM_LEDS];
 CRGBPalette16 gPal;
 
 #include "GyverTimer.h"
-GTimer_ms effectTimer(60);
 GTimer_ms autoplayTimer((long)AUTOPLAY_TIME * 1000);
-GTimer_ms brightTimer(20);
 
-int brightness = BRIGHTNESS;
-int tempBrightness;
-byte thisMode;
+static int brightness = BRIGHTNESS;
+static boolean autoplay = true;
 
-bool gReverseDirection = false;
-boolean loadingFlag = true;
-boolean autoplay = true;
-boolean powerDirection = true;
-boolean powerActive = false;
-boolean powerState = true;
-boolean whiteMode = false;
-boolean brightDirection = true;
-boolean wasStep = false;
+enum ModeWork
+{
+    Static = 1,
+    Dynamic
+};
 
-// залить все
+static ModeWork modeWork = Static;
+
+// Задать всем светодиодам один цвет
 void fillAll(CRGB newcolor)
 {
     for (int i = 0; i < NUM_LEDS; i++)
@@ -69,15 +51,30 @@ void fillAll(CRGB newcolor)
     }
 }
 
-// функция получения цвета пикселя по его номеру
-uint32_t getPixColor(int thisPixel)
+// ****************************** ЦВЕТА ******************************
+void effectColors()
 {
-    return (((uint32_t)leds[thisPixel].r << 16) | ((long)leds[thisPixel].g << 8) | (long)leds[thisPixel].b);
+    static byte hue = 0;
+    hue += 2;
+    CRGB thisColor = CHSV(hue, 255, 255);
+    fillAll(CHSV(hue, 255, 255));
+}
+
+// ****************************** РАДУГА ******************************
+void effectRainbow()
+{
+    static byte hue = 0;
+    hue += 2;
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        leds[i] = CHSV((byte)(hue + i * float(255 / NUM_LEDS)), 255, 255);
+    }
 }
 
 void setup()
 {
     Serial.begin(9600);
+    pinMode(IDS1820_PIN, OUTPUT);
     FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
     if (CURRENT_LIMIT > 0)
     {
@@ -86,26 +83,8 @@ void setup()
     FastLED.setBrightness(brightness);
     FastLED.show();
 
-    randomSeed(analogRead(0));
     touch.setTimeout(300);
     touch.setStepTimeout(50);
-
-    if (FIRE_PALETTE == 0)
-    {
-        gPal = HeatColors_p;
-    }
-    else if (FIRE_PALETTE == 1)
-    {
-        gPal = CRGBPalette16(CRGB::Black, CRGB::Red, CRGB::Yellow, CRGB::White);
-    }
-    else if (FIRE_PALETTE == 2)
-    {
-        gPal = CRGBPalette16(CRGB::Black, CRGB::Blue, CRGB::Aqua, CRGB::White);
-    }
-    else if (FIRE_PALETTE == 3)
-    {
-        gPal = CRGBPalette16(CRGB::Black, CRGB::Red, CRGB::White);
-    }
 }
 
 void loop()
@@ -115,167 +94,30 @@ void loop()
     // Управление режимами
     if (touch.hasClicks())
     {
-        byte clicks = touch.getClicks();
-
-        switch (clicks)
-        {
-        case 1:
-            powerDirection = !powerDirection;
-            powerActive = true;
-            tempBrightness = brightness * !powerDirection;
-            break;
-
-        case 2:
-            if (!whiteMode && !powerActive)
-            {
-                nextMode();
-            }
-            break;
-
-        case 3:
-            if (!powerActive)
-            {
-                whiteMode = !whiteMode;
-                if (whiteMode)
-                {
-                    effectTimer.stop();
-                    fillAll(CRGB::White);
-                    FastLED.show();
-                }
-                else
-                {
-                    effectTimer.start();
-                }
-            }
-            break;
-
-        case 4:
-            if (!whiteMode && !powerActive)
-            {
-                autoplay = !autoplay;
-            }
-            break;
-
-        default:
-            break;
-        }
-    }
-
-    // Управление яркостью
-    if (touch.isStep())
-    {
-        if (!powerActive)
-        {
-            wasStep = true;
-            if (brightDirection)
-            {
-                brightness += 5;
-            }
-            else
-            {
-                brightness -= 5;
-            }
-            brightness = constrain(brightness, MIN_BRIGHTNESS, 255);
-            FastLED.setBrightness(brightness);
-            FastLED.show();
-        }
-    }
-
-    // Отпускание сенсора
-    if (touch.isRelease())
-    {
-        if (wasStep)
-        {
-            wasStep = false;
-            brightDirection = !brightDirection;
-        }
-    }
-
-    // Режим свечения
-    if (effectTimer.isReady() && powerState)
-    {
-        switch (thisMode)
-        {
-        case 0:
-            lighter();
-            break;
-
-        case 1:
-            lightBugs();
-            break;
-
-        case 2:
-            colors();
-            break;
-
-        case 3:
-            rainbow();
-            break;
-
-        case 4:
-            sparkles();
-            break;
-
-        case 5:
-            fire();
-            break;
-
-        case 6:
-            vinigret();
-            break;
-        }
+        // Включаем воспроизведение голосовой записи
+        digitalWrite(IDS1820_PIN, HIGH);
+        // Переходим в измененный режим работы светодиодов
+        modeWork = Dynamic;
         FastLED.show();
     }
 
+    switch (modeWork)
+    {
+    case Static:
+        effectColors();
+        break;
+
+    case Dynamic:
+        effectRainbow();
+        break;
+
+    default:
+        break;
+    }
+
     if (autoplayTimer.isReady() && autoplay)
-    { // таймер смены режима
-        nextMode();
-    }
-
-    brightnessTick();
-}
-
-void nextMode()
-{
-    thisMode++;
-    if (thisMode >= MODES_AMOUNT)
     {
-        thisMode = 0;
-    }
-    loadingFlag = true;
-    FastLED.clear();
-}
-
-void brightnessTick()
-{
-    if (powerActive)
-    {
-        if (brightTimer.isReady())
-        {
-            if (powerDirection)
-            {
-                powerState = true;
-                tempBrightness += 10;
-                if (tempBrightness > brightness)
-                {
-                    tempBrightness = brightness;
-                    powerActive = false;
-                }
-                FastLED.setBrightness(tempBrightness);
-                FastLED.show();
-            }
-            else
-            {
-                tempBrightness -= 10;
-                if (tempBrightness < 0)
-                {
-                    tempBrightness = 0;
-                    powerActive = false;
-                    powerState = false;
-                }
-                FastLED.setBrightness(tempBrightness);
-                FastLED.show();
-            }
-        }
+        modeWork = Static;
+        digitalWrite(IDS1820_PIN, LOW);
     }
 }
